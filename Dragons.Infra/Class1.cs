e21d44f53ab74f6a17e36cde1fd0c3e2785d2fad
Diego Fernandes <dondesigner@hotmail.com>
2017-07-07 18:04:26 -0300

    Iniciando o Projeto
    Origem do tutor: http://www.primaryobjects.com/

---
+++ Dragons.Infra/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dragons.Infra
+{
+    public class Class1
+    {
+    }
+}
